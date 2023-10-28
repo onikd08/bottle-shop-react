dca236197bb71e75abe48b3a039be95f791dc7cf
Anik Das <onikd08@gmail.com>
2023-10-28 05:39:11 +0300

    Load bottles from fakedata

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import Bottles from "./components/Bottles/Bottles";
-import SingleBottle from "./components/SingleBottle/SingleBottle";
 
 function App() {
   return (
     <>
       <Bottles></Bottles>
-      <SingleBottle></SingleBottle>
     </>
   );
 }
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+
 const Bottles = () => {
-  return <div>Bottles</div>;
+  const [bottles, setBottles] = useState([]);
+
+  useEffect(() => {
+    const loadBottles = async () => {
+      const res = await fetch("bottles.json");
+      const data = await res.json();
+      setBottles(data);
+    };
+    loadBottles();
+  }, []);
+  return (
+    <div>
+      <h2>Total Bottles: {bottles.length}</h2>
+    </div>
+  );
 };
 
 export default Bottles;
